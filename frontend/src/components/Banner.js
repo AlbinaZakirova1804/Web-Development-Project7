f7228b1f1bb370a9a8b7265b37d98be12f51c40f
AlbinaZakirova1804 <zak1804@gmail.com>
2021-06-16 15:07:08 -0400

    started with react

---
+++ frontend/src/components/Banner.js
@@ -0,0 +1,8 @@
+  import '../styles/banner.css';
+  function Banner() {
+    return <div class='js-banner'>
+      <h1>Work talk (Messager)</h1>
+      </div>
+}
+
+export default Banner
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,15 @@
+import '../styles/login.css'
+
+<input type="email"></input>
+function Login() {
+    return <div class='input'>
+                <h3>User login</h3>
+
+                    
+        <input type="email"></input>
+        <input type="password"></input>
+                    
+            </div>
+}
+
+export default Login
